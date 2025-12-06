495d3d9cec2baf99bc8f777f511ae41eb12705b1
Aaron George Abraham <138325645+Aaron-53@users.noreply.github.com>
2025-12-06 08:44:00 +0530

    Update welcome message in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,9 @@ import { QrScanner } from '@/components/qr-scanner';
 export default function Home() {
   return (
     <main className="flex min-h-screen w-full flex-col items-center justify-center bg-background p-4 md:p-8">
-      <h1 className="sr-only">QR Scanner Notifier</h1>
+      <h1 className="sr-only">Welcome to Omega</h1>
       <p className="sr-only">
-        Point your camera at a QR code to register its content.
+        Scan your QR to CheckIn.
       </p>
       <QrScanner />
     </main>
